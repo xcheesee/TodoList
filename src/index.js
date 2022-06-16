0f09a5e120fcaf0c3f2faa929490afedbb179aa0
Lucas-vm <comecaste@gmail.com>
2022-06-16 19:36:49 -0300

    reformatted code, added css code and

--- src/index.js
+++ src/index.js
@@ -6,88 +6,78 @@
 // D - DEPENDENCY INVERSION
 // DRY - DONT REPEAT YOURSELF
 
-
 import './style.css';
 
 const addBtn = document.querySelector('#addBtn')
 addBtn.onclick = showForm
+
+function showForm (event) {
+    buildForm.appendFormQuestions(questionItems.getNames() , questionItems.getClasses(), questionItems.getTypes(), questionItems.getTags(), questionItems.getOptions())
+    document.body.appendChild(buildForm.container)
+    event.preventDefault();
+    return
+}
 
 const buildForm = (() => {
     const container = document.createElement('div')
     container.setAttribute('id', 'formContainer')
     let formArea;
 
-    
-    const appendItems = (names, ids, nodes, types) => {
+    const appendFormQuestions = (names, ids, types, tags, options) => {
         formArea = document.createElement('form')
-        // formArea.setAttribute('onsubmit', 'return false')
+        formArea.setAttribute('onsubmit', 'return false') //for debuging porpuses
         formArea.setAttribute('action' , '')
         formArea.setAttribute('id', 'form')
-        for(let i = 0; i < nodes.length; i++) {
-            let node = document.createElement('input')
-            node.setAttribute('id',`${ids[i]}`)
-            node.setAttribute('name', `${ids[i]}`)
-            node.setAttribute('type', `${types[i]}`)
+
+        for(let i = 0; i < ids.length; i++) {
+            let node;
+            if(tags[i] === 'select') {
+                node = makeDomObject.makeSelect(tags[i], types[i], names[i], ids[i], options[i])
+            }
+            else if(tags[i] === 'input') {
+                node = makeDomObject.makeInput(tags[i], types[i], names[i], ids[i])
+            }
             let label = document.createElement('label')
             label.setAttribute('for', `${ids[i]}`)
             label.innerText = `${names[i]}`
-            formArea.appendChild(label)
-            formArea.appendChild(node)
+            addToPage(formArea, label, node)
         }
+
         let submitBtn = createBtn('submit', 'Submit')
-        formArea.appendChild(submitBtn)
-        formArea.appendChild(createBtn('reset', 'Clear'))
-        formArea.addEventListener('submit', submitItems)
-        // submitBtn.addEventListener('click', () => document.body.removeChild(container))
-        container.appendChild(formArea)
+        formArea.addEventListener('submit', submitFormQuestions)
+
+        addToPage(formArea, submitBtn, createBtn('reset', 'Clear'))
+        addToPage(container, formArea)
     }
 
-    return {container, appendItems}
+    return {container, appendFormQuestions}
 })();
 
-
-function showForm (event) {
-    buildForm.appendItems(questionItems.getNames() , questionItems.getClasses(), questionItems.getNodes(), questionItems.getTypes())
-    document.body.appendChild(buildForm.container)
-    event.preventDefault();
-    return
-}
-
-function submitItems(event) {
+function submitFormQuestions(event) {
     const formData = new FormData(event.target)
     let data = Object.fromEntries(formData)
+
+    if (data.title == '' && data.desc == '') {
+        return
+    }
+    
     pageData.addNote(data)
-    console.log(data)
-    addToPage(document.body, data.title, data.desc, data.dueD, data.priority, data.dataId)
+    console.log(Object.entries(data))
+    let note = makeDomObject.makeNote(Object.entries(data), data.dataId)
+    document.body.appendChild(note)
     event.preventDefault();
+    buildForm.container.removeChild(document.querySelector('form'))
     document.body.removeChild(buildForm.container)
     return
 }
 
-function createNote (title, desc, date, priority, id) {
-    let noteCard = document.createElement('div')
-    let noteTitle = document.createElement('h2')
-    noteTitle.innerText = `${title}`
-    let noteDesc = document.createElement('p')
-    noteDesc.innerText = `${desc}`
-    let noteDate = document.createElement('p')
-    noteDate.innerText = `${date}`
-    let notePrio = document.createElement('p')
-    notePrio.innerText = `${priority}`
-    noteCard.appendChild(noteTitle)
-    noteCard.appendChild(noteDesc)
-    noteCard.appendChild(noteDate)
-    noteCard.appendChild(notePrio)
-    noteCard.setAttribute('id', `card${id}`)
-    return noteCard
-}
-
-function addToPage (container, title, desc, date, priority, id) {
+function addToPage (container, ...childNodes) {
     // if(!item) {
     //     warnEmptyList(container)
     // }
-    let note = createNote(title, desc, date, priority, id)
-    container.appendChild(note)
+    for(let node of childNodes) {
+        container.appendChild(node)
+    }
     return
 }
 
@@ -99,18 +89,19 @@ function addToPage (container, title, desc, date, priority, id) {
 const pageData = (() => {
     let notesData = [];
     let id = 0;
+
     const addNote = (item) => {
         item.dataId = id
         notesData.push(item)
-        console.log(notesData)
         id++;
     }
+
     const getNotes = () => notesData
 
     return {addNote, getNotes}
 })();
 
-function createBtn(type, name) {
+const createBtn = (type, name) => {
     let btn = document.createElement('button')
     btn.setAttribute('type', type)
     btn.setAttribute('id', type)
@@ -120,41 +111,99 @@ function createBtn(type, name) {
 
 const questionItems = (() => {
     let formItems = []
+
     const addFormItem = (...items) => {
         items.forEach(item => formItems.push(item))
     }
-    const getNodes = () => formItems.map(item => item.node)
     const getNames = () => formItems.map(item => item.name)
     const getClasses = () => formItems.map(item => item.id)
     const getTypes = () => formItems.map(item => item.type)
-    return {formItems, addFormItem, getNodes, getNames, getClasses, getTypes}
+    const getTags = () => formItems.map(item => item.tag)
+    const getOptions = () => formItems.map(item => item.options)
+    return {formItems, addFormItem, getNames, getClasses, getTypes, getTags, getOptions}
 })();
 
+const makeDomObject = (() => {
+    let node;
 
-const title = {
-    name: 'Title',
-    id: 'title',
-    type: 'text',
-    node: document.createElement('input')
-}
-const desc = {
-    name: 'Description',
-    id: 'desc',
-    type: 'text',
-    node: document.createElement('input')
-}
-const dueDate = {
-    name: 'Due date',
-    id: 'dueD',
-    type: 'text',
-    node: document.createElement('input')
-}
-const priority = {
-    name: 'Priority',
-    id: 'priority',
-    type: 'text',
-    node: document.createElement('input')
-}
+    const makeSelect = (tag, type, name, id, options) => {
+        node = document.createElement(`${tag}`)
+        node.setAttribute('id',`${id}`)
+        node.setAttribute('name', `${id}`)
+        node.setAttribute('type', `${type}`)
 
-questionItems.addFormItem(title, desc, dueDate, priority)
+        for(let option of options) {
+            let opNode = document.createElement('option')
+            opNode.setAttribute('value', `${option}`)
+            opNode.innerText = `${option}`
+            addToPage(node, opNode)
+        }
+
+        return node
+    }
+
+    const makeInput = (tag, type, name, id) => {
+        node = document.createElement(`${tag}`)
+        node.setAttribute('id',`${id}`)
+        node.setAttribute('name', `${id}`)
+        node.setAttribute('type', `${type}`)
+
+        return node
+    }
+
+    const makeNote = (formArray, formId) => {
+        node = document.createElement('div')
+        let nodeChildren = []
+
+        for(let array of formArray) {
+            if(array[0] === 'dataId') {
+                continue
+            }
+            let note = document.createElement('p')
+            note.classList.add(`${array[0]}`)
+            note.innerText = `${array[1]}`
+            nodeChildren.push(note)
+        }
+
+        addToPage(node, ...nodeChildren)
+        node.setAttribute('id', `card${formId}`)
+
+        return node
+    }
+    return {makeSelect, makeInput, makeNote, node}
+})();
+
+const createQuestion = (() => {
+    let question;
+
+    const text = (name, id) => {
+        question = {};
+        question.tag = 'input'
+        question.type = 'text'
+        question.name = name
+        question.id = id
+        questionItems.addFormItem(question)
+        return
+    }
+
+    const select = (name, id, options) => {
+        question = {};
+        question.tag = 'select'
+        question.type = ''
+        question.name = name
+        question.id = id
+        question.options = options
+        questionItems.addFormItem(question)
+        return
+    }
+
+    return {text, select}
+})();
+
+createQuestion.text('Title', 'title')
+createQuestion.text('Description', 'desc')
+createQuestion.text('Due Date', 'dueD')
+createQuestion.text('Test', 'test')
+createQuestion.select('Priority', 'priority', ['low', 'medium', 'high'])
+
 console.log(questionItems.formItems)
