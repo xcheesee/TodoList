9a14f8d6d7194051ca04848f4997f0ec400420dd
Lucas-vm <comecaste@gmail.com>
2022-06-27 20:36:23 -0300

    minor changes

--- src/index.js
+++ src/index.js
@@ -7,261 +7,273 @@
 // DRY - DONT REPEAT YOURSELF
 
 import './style.css';
-import isValid from 'date-fns/isValid'
-import isPast from 'date-fns/isPast'
+// import isValid from 'date-fns/isValid';
+// import isPast from 'date-fns/isPast';
 import Datepicker from 'vanillajs-datepicker/Datepicker';
-import { ka } from 'date-fns/locale';
+// import { ka } from 'date-fns/locale';
 
-const mainPage = document.querySelector('.mainPage')
-const notesForm = document.querySelector('.nFormWrapper')
-const projsForm = document.querySelector('.pFormWrapper')
-const cardContainer = document.querySelector('.cardContainer')
-const addBtn = document.querySelector('#notesBtn')
-const projBtn = document.querySelector('#projBtn')
-addBtn.onclick = showNotesForm
-projBtn.onclick = showProjForm
-const nForm = document.querySelector('#nForm')
-const pForm = document.querySelector('#pForm')
-nForm.addEventListener('submit', submitNoteQuestions)
-pForm.addEventListener('submit', submitProjsQuestions)
+const notesForm = document.querySelector('.nFormWrapper');
+const projsForm = document.querySelector('.pFormWrapper');
+const addBtn = document.querySelector('#notesBtn');
+const projBtn = document.querySelector('#projBtn');
+const nForm = document.querySelector('#nForm');
+const pForm = document.querySelector('#pForm');
 let currCardContainer;
 
+// eslint-disable-next-line no-unused-vars
 const datepicker = new Datepicker(document.querySelector('#date'), {
     autohide: true,
     format: 'dd/mm/yyyy'
-})
+});
 
 function showNotesForm () {
-    attachProjs(pageData.getProjs())
-    notesForm.classList.toggle('showForm')
-    return
+    attachProjs(pageData.getProjs());
+    notesForm.classList.toggle('showForm');
+    return;
 }
 
 function showProjForm () {
-    projsForm.classList.toggle('showForm')
-    return
+    projsForm.classList.toggle('showForm');
+    return;
 }
 
 function attachProjs (projObjValues) {
-    const projSelect = document.querySelector('#projSelect')
-    clearPage(projSelect)
-    let projDomValues = makeDomObject.makeOptions(projObjValues)
-    projDomValues.forEach(dom => projSelect.appendChild(dom))
-    return
+    const projSelector = document.querySelector('#projSelect');
+    clearDom(projSelector);
+    let projDomValues = makeDomObject.makeOptions(projObjValues);
+    projDomValues.forEach(dom => projSelector.appendChild(dom));
+    return;
 }
 
-function submitNoteQuestions (event) {
-    const formData = new FormData(event.target)
-    let data = Object.fromEntries(formData)
-    
+function addNote (event) {
+    event.preventDefault();
+
+    let data = Object.fromEntries(new FormData(event.target));
+
     pageData.addNote(data)
-    makeDomObject.updateProjTabEle(data.proj)
-    // cardContainer.appendChild(makeDomObject.makeNote(data))
-    if(currCardContainer == data.proj) {
-        showProjectNotes(data.proj)
-    }
-    notesForm.classList.toggle('showForm')
-    event.preventDefault();
-    event.target.reset()
-    return
+      .then(makeDomObject.updateProjTabEle);
+
+    if (currCardContainer === data.proj) showProjectNotes(pageData.getProject(data.proj));
+
+    notesForm.classList.toggle('showForm');
+    event.target.reset();
+    return;
 }
 
-function submitProjsQuestions (event) {
-    const projSidebar = document.querySelector('.sidebar')
-    const formData = new FormData(event.target)
-    let data = Object.fromEntries(formData)
-    console.log(data)
-    if(pageData.duplicateProj(data.name)) return
-    pageData.addProj(data)
-    addToPage(projSidebar, makeDomObject.makeProjTabEle(data.name))
-    projsForm.classList.toggle('showForm')
+function addProject (event) {
     event.preventDefault();
-    event.target.reset()
-    return
+
+    if (pageData.duplicateProj(event.target.name.value)) {
+        projsForm.classList.toggle('showForm');
+        event.target.reset();
+        return;
+    }
+
+    const projSidebar = document.querySelector('.sidebar');
+
+    let data = Object.fromEntries(new FormData(event.target));
+
+    pageData.addProj(data)
+      .then(addToPage(projSidebar, makeDomObject.makeProjTabEle(data.name)))
+
+    projsForm.classList.toggle('showForm');
+    event.target.reset();
+    return;
 }
 
 function addToPage (container, ...childNodes) {
-    return childNodes.forEach(node => container.appendChild(node))
+    return childNodes.forEach(node => container.appendChild(node));
 }
 
 const pageData = (() => {
-    let notesData = [];
     let projectsData = {
         default: {
             name: 'default',
             toDos: [],
             id: 0
         }
-    }
+    };
     let noteId = 0;
     let projId = 1;
 
     const addNote = (item) => {
-        item.dataId = noteId
-        projectsData[item.proj].toDos.push(item)
-        console.log(item, projectsData)
-        noteId++;
-    }
+        return new Promise((resolve, reject) => {
+            if(projectsData[item.proj]) {
+                item.dataId = noteId;
+                projectsData[item.proj].toDos.push(item);
+                noteId++;
+                resolve(item.proj)
+            }
+            else reject('Error: project not found')
+        })
+
+    };
 
     const addProj = (proj) => {
-        if(projectsData[proj.name]) return false
-        projectsData[proj.name] = {
-            name: proj.name,
-            toDos: [],
-            id: projId
-        }
-        projId++;
-        console.log(projectsData)
-        console.log(getProjs())
-        return true
-    }
+        return new Promise((resolve, reject) => {
+            if(projectsData[proj.name]) reject('Project already on page')
 
-    const getNotes = () => notesData
+            projectsData[proj.name] = {
+                name: proj.name,
+                toDos: [],
+                id: projId
+            };
+            projId++;
+            resolve(proj.name)
+        })
+
+
+    };
 
     const getProjs = () => {
-        let projNames = []
+        let projNames = [];
         for(let project in projectsData) {
-            projNames.push(projectsData[project].name)
+            projNames.push(projectsData[project].name);
         }
-        return projNames
-    }
+        return projNames;
+    };
 
-    const getProjNotes = (proj) => projectsData[proj].toDos
+    const getProjNotes = (proj) => projectsData[proj].toDos;
 
-    const getProjData = (proj) => projectsData[proj]
+    const getProject = (proj) => projectsData[proj];
 
     const duplicateProj = (proj) => {
         if (projectsData[proj]) {
-            return true
+            return true;
             //add error message
         }
-        return false
-    }
+        return false;
+    };
 
-    return {addNote, getNotes, addProj, getProjs, getProjNotes, duplicateProj, getProjData}
+    return {addNote, addProj, getProjs, getProjNotes, duplicateProj, getProject};
 })();
 
 const makeDomObject = (() => {
     let node;
 
     const makeNote = ({title, desc, date, prio, dataId}) => {
-        node = document.createElement('div')
-        node.setAttribute('id', `card${dataId}`)
-        node.classList.add('card')
+        node = document.createElement('div');
+        node.setAttribute('id', `card${dataId}`);
+        node.classList.add('card');
 
-        const t = document.createElement('h2')
-        t.setAttribute('id', 'cardTitle')
-        t.innerText = `${title}`
+        const t = document.createElement('h2');
+        t.setAttribute('id', 'cardTitle');
+        t.innerText = `${title}`;
 
-        const d = document.createElement('p')
-        d.setAttribute('id', 'cardDesc')
-        d.innerText = `${desc}`
+        const d = document.createElement('p');
+        d.setAttribute('id', 'cardDesc');
+        d.innerText = `${desc}`;
 
-        const dueDate = document.createElement('p')
-        dueDate.setAttribute('id', `cardDate`)
-        dueDate.innerText = `${date}`
+        const dueDate = document.createElement('p');
+        dueDate.setAttribute('id', `cardDate`);
+        dueDate.innerText = `${date}`;
 
-        const priority = document.createElement('p')
-        priority.setAttribute('id', 'cardPrio')
-        priority.innerText = `${prio} priority`
+        const priority = document.createElement('p');
+        priority.setAttribute('id', 'cardPrio');
+        priority.innerText = `${prio} priority`;
         
-        addToPage(node, t, d, dueDate, priority)
+        addToPage(node, t, d, dueDate, priority);
 
-        return node
-    }
+        return node;
+    };
 
     const makeOptions = (values) => {
         let domItems = values.map(value => {
-            let domValue = document.createElement('option')
-            domValue.setAttribute('value', value)
-            domValue.innerText = capitalized(value)
-            return domValue
-        })
-        return domItems
-    }
+            let domValue = document.createElement('option');
+            domValue.setAttribute('value', value);
+            domValue.innerText = capitalized(value);
+            return domValue;
+        });
+        return domItems;
+    };
 
     const makeProjTabEle = (projectName) => {
-        let sidebarEle = document.createElement('p')
-        sidebarEle.setAttribute('id', `${projectName}`)
-        sidebarEle.innerText = `${projectName}--------${pageData.getProjNotes(projectName).length}`
-        sidebarEle.addEventListener('click', getProjectNotes)
-        return sidebarEle
-    }
+        let sidebarEle = document.createElement('p');
+        sidebarEle.setAttribute('id', `${projectName}`);
+        sidebarEle.innerText = `${projectName}--------${pageData.getProjNotes(projectName).length}`;
+        sidebarEle.addEventListener('click', getProjectNotes);
+        return sidebarEle;
+    };
 
     const updateProjTabEle = (projectName) => {
-        let sidebarEle = document.querySelector(`#${projectName}`)
-        sidebarEle.innerText = `${projectName}--------${pageData.getProjNotes(projectName).length}`
-        return
-    }
+        let sidebarEle = document.querySelector(`#${projectName}`);
+        sidebarEle.innerText = `${projectName}--------${pageData.getProjNotes(projectName).length}`;
+        return;
+    };
 
     const initSidebar = () => {
         for(let proj of pageData.getProjs()) {
-            addToPage(document.querySelector('.sidebar'), makeProjTabEle(proj))
+            addToPage(document.querySelector('.sidebar'), makeProjTabEle(proj));
         }
-        return
-    }
+        return;
+    };
 
-    return {makeNote, makeOptions, makeProjTabEle, updateProjTabEle, initSidebar}
+    return {makeNote, makeOptions, makeProjTabEle, updateProjTabEle, initSidebar};
 })();
 
-function isFormValid ({title, desc, dueD}) {
-    let [day, month, year] = dueD.split('/')
-    month--;
-    //23:59:59 to be able to define note for same day
-    let fDate = new Date(year, month , day, 23, 59, 59)
-    console.log(fDate)
+// function isFormValid ({title, desc, dueD}) {
+//     let [day, month, year] = dueD.split('/');
+//     month--;
+//     //23:59:59 to be able to define note for same day
+//     let fDate = new Date(year, month , day, 23, 59, 59);
 
-    if(title === '') {
-        let title = document.querySelector('#title')
-        title.classList.add('failTitle')
-        return false
-    } else if (desc === '') {
-        let desc = document.querySelector('#desc')
-        desc.classList.add('failDesc')
-        return false
-    } else if (!isValid(fDate)) {
-        let d = document.querySelector('#dueD')
-        d.classList.remove('pastDate')
-        d.classList.add('blankDate')
-        return false
-    } else if (isPast(fDate)) {
-        let d = document.querySelector('#dueD')
-        d.classList.remove('blankDate')
-        d.classList.add('pastDate')
-        return false
-    }
+//     if(title === '') {
+//         let title = document.querySelector('#title');
+//         title.classList.add('failTitle');
+//         return false;
+//     } else if (desc === '') {
+//         let desc = document.querySelector('#desc');
+//         desc.classList.add('failDesc');
+//         return false;
+//     } else if (!isValid(fDate)) {
+//         let d = document.querySelector('#dueD');
+//         d.classList.remove('pastDate');
+//         d.classList.add('blankDate');
+//         return false;
+//     } else if (isPast(fDate)) {
+//         let d = document.querySelector('#dueD');
+//         d.classList.remove('blankDate');
+//         d.classList.add('pastDate');
+//         return false;
+//     }
 
-    return true
-}
+//     return true;
+// }
 
 function capitalized (value) {
-    let str = value
+    let str = value;
     let str2 = str.charAt(0).toUpperCase() + str.slice(1);
-    return str2
+    return str2;
 }
 
-function clearPage (element) {
+function clearDom (element) {
     while (element.firstChild) {
-       element.removeChild(element.firstChild)
+       element.removeChild(element.firstChild);
     }
 }
 
 function getProjectNotes (event) {
-    const project = event.target.id
-    currCardContainer = project
-    showProjectNotes(project)
+    const project = event.target.id;
+    currCardContainer = project;
+    showProjectNotes(pageData.getProject(project));
 }
 
-function showProjectNotes (data) {
-    const projectData = pageData.getProjData(data)
-    const container = document.querySelector('.cardContainer')
-    const projCont = document.createElement('div')
-    projCont.setAttribute('id', `${data}Container`)
-    projCont.innerText = `${data} Project`
-    projectData.toDos.forEach(obj => addToPage(projCont, makeDomObject.makeNote(obj)))
-    clearPage(container)
-    container.appendChild(projCont)
+function showProjectNotes ({name, toDos}) {
+
+    const container = document.querySelector('.cardContainer');
+    const projCont = document.createElement('div');
+    projCont.setAttribute('id', `${name}Container`);
+    projCont.innerText = `${name} Project`;
+    toDos.forEach(obj => addToPage(projCont, makeDomObject.makeNote(obj)));
+    clearDom(container);
+    container.appendChild(projCont);
 
 }
 
-makeDomObject.initSidebar()
+nForm.addEventListener('submit', addNote);
+pForm.addEventListener('submit', addProject)
+projBtn.onclick = showProjForm;
+addBtn.onclick = showNotesForm;
+
+makeDomObject.initSidebar();
+showProjectNotes(pageData.getProject('default'));
